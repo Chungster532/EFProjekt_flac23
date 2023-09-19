6695328b8b9c8107ab56b524e9eb641187387525
Cyrill Valentini <cyrill.valentini@outlook.com>
2023-09-19 10:44:01 +0200

    moved templates into frontend folder

---
+++ frontend/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+
+app = Flask(__name__)
+app.secret_key = "dYVXfvWUUywT86uvSFzwdM19Nk3RNJ"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["userid"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
