86f8837e7ae046d24aea7fad3c0ae12f81f776fc
fabian0702 <loris.hirter@quickline.ch>
2023-09-19 17:59:13 +0200

    Added Authentication and migrated to flask

--- backend/database.py
+++ backend/database.py
@@ -26,10 +26,12 @@ class user:
         self.username = username
         self.description = description
         self.image = image
-    def __iter__(self):
-        return iter((self.id, self.username, self.description, self.image))
-    def __dict__(self) -> dict:
-        return {'id':self.id, 'username':self.username, 'description':self.description, 'image':self.image}
+
+def userToDict(id, username, passwordHash, description, image, **args):
+    return {'id':id, 'username':username, 'passwordHash': passwordHash, 'description':description, 'image':image}
+
+def postToDict(id, userId, title, image, description, timestamp):
+    return {'id': id, 'userId': userId, 'title': title, 'image':image, 'description':description, 'timestamp':str(datetime.datetime.fromtimestamp(timestamp))}
 
 class DB:
     def __init__(self) -> None:
@@ -38,32 +40,45 @@ class DB:
         self.cur.execute('''CREATE TABLE IF NOT EXISTS users ( id TEXT PRIMARY KEY NOT NULL UNIQUE, username TEXT NOT NULL UNIQUE, passwordhash TEXT NOT NULL, description TEXT NOT NULL, image TEXT NOT NULL );''')
         
         self.cur.execute('''CREATE TABLE IF NOT EXISTS posts ( id TEXT PRIMARY KEY NOT NULL UNIQUE, userId TEXT NOT NULL, title TEXT NOT NULL, image TEXT NOT NULL, description TEXT NOT NULL, creationDate TEXT NOT NULL );''')
-    def add_post(self, post:post) -> post:
+    def add_post(self, *args) -> dict[str, str]:
         """Function to add post to the db"""
         #try:
-        self.cur.execute("INSERT INTO posts VALUES(?,?,?,?,?,?)", tuple(post))
+        self.cur.execute("INSERT INTO posts VALUES(?,?,?,?,?,?)", args)
         print('\n'.join(list(self.db.iterdump())))
         self.db.commit()
-        return self.get_post_by_id(post.id)
-        #except Exception as e:
-        #    print(e)
-        #    return None
-    def get_post_by_id(self, id:str) -> post:
+        return self.get_post_by_id(args[0])
+    def add_user(self, *args) -> dict[str, str]:
+        """Function to add user to the db"""
+        self.cur.execute("INSERT INTO users VALUES(?,?,?,?,?)", args)
+        print('\n'.join(list(self.db.iterdump())))
+        self.db.commit()
+        return self.get_user_by_name(args[1])
+    def get_post_by_id(self, id:str) -> dict[str, str]:
         """return a list containing all attributes of a post specified b an uuid"""
         print(f'fetching id {id}')
-        pst =  self.cur.execute("""SELECT * FROM posts WHERE id=?""", (str(id),)).fetchone()
-        print(pst, post(*pst) if pst else None)
-        return post(*pst) if pst else None
-
-    def get_all_posts(self, count:int, offset:int) -> list[post]:
-        """function which return all posts"""
-        return [post(*pst) if pst else None for pst in self.cur.execute("""SELECT * FROM posts LIMIT ? OFFSET ?""", (str(count), str(offset),)).fetchall()]
+        pst =  self.cur.execute("""SELECT * FROM posts WHERE id=?""", (id,)).fetchone()
+        return postToDict(*pst) if pst else None
     
-    def searchUser(self, name:str) -> list[user]:
+    def get_user_by_name(self, name:str) -> dict[str, str]:
+        """return a list containing all attributes of a post specified b an uuid"""
+        print(f'fetching id {name}')
+        pst =  self.cur.execute("""SELECT * FROM users WHERE username=?""", (name,)).fetchone()
+        return userToDict(*pst) if pst else None
+    def get_user_by_id(self, id:str) -> dict[str, str]:
+        """return a list containing all attributes of a post specified b an uuid"""
+        print(f'fetching id {id}')
+        pst =  self.cur.execute("""SELECT * FROM users WHERE id=?""", (id,)).fetchone()
+        return userToDict(*pst) if pst else None
+
+    def get_all_posts(self, count:int, offset:int) -> list[dict[str, str]]:
+        """function which return all posts"""
+        return [postToDict(*pst) if pst else None for pst in self.cur.execute("""SELECT * FROM posts LIMIT ? OFFSET ?""", (str(count), str(offset),)).fetchall()]
+    
+    def searchUser(self, name:str) -> list[dict[str, str]]:
         """Function to search for a user by name"""
-        [user(*usr) if usr else None for usr in self.cur.execute("""SELECT * FROM posts WHERE username LIKE ?""", ('%'+name+'%',)).fetchall()]
+        [userToDict(usr) if usr else None for usr in self.cur.execute("""SELECT * FROM posts WHERE username LIKE ?""", ('%'+name+'%',)).fetchall()]
         return 
     
-    def searchPosts(self, name:str) -> list[post]:
+    def searchPosts(self, name:str) -> list[dict[str, str]]:
         """Function to search posts by prompt"""
-        return [post(*pst) if pst else None for pst in self.cur.execute("""SELECT * FROM posts WHERE title LIKE ? OR description LIKE ?""", ('%'+name+'%', '%'+name+'%')).fetchall()]
+        return [postToDict(*pst) if pst else None for pst in self.cur.execute("""SELECT * FROM posts WHERE title LIKE ? OR description LIKE ?""", ('%'+name+'%', '%'+name+'%')).fetchall()]
