15971329f7dfa286611d85ecd0bba9f10bddaa49
fabian0702 <loris.hirter@quickline.ch>
2023-09-19 22:35:13 +0200

    Added Posting

--- frontend/main.py
+++ frontend/main.py
@@ -62,8 +62,9 @@ def login():
     
 @app.route("/logout/")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+    resp = redirect(url_for("login"))
+    resp.set_cookie('Session-Cookie=""')
+    return resp
 
 @app.route("/createpost/")
 def createpost():
