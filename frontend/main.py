7857ff21635c00e7a56c724428ed12f378c924b4
Cyrill Valentini <cyrill.valentini@outlook.com>
2023-09-19 18:53:18 +0200

    added support for user specific post on account page

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
-from backend.api import app as api
+#from backend.api import app as api
 
 with open('frontend/mountain.txt', 'r') as f:
     img = f.read()
@@ -26,7 +26,7 @@ response = [
 
 print
 
-def generate_html(response):
+def generate_feed(response):
     html_content = "<html><head><title>Post List</title></head><body>"
     
     for post in response:
@@ -41,7 +41,24 @@ def generate_html(response):
     
     html_content += "</body></html>"
     
-    with open('frontend/templates/postTemplate.html', 'w') as html_file:
+    with open('frontend/templates/tempfeed.html', 'w') as html_file:
+        html_file.write(html_content)
+
+def generate_profile(response):
+    html_content = "<html><head><title>Post List</title></head><body>"
+    
+    for post in response:
+        html_content += "<div style='border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;'>"
+        html_content += f"<div style='max-width: 400px; margin: 0 auto;'>"
+        html_content += f"<img src='{post['image']}' alt='Post Image' style='max-width: 100%;'><br>"
+        html_content += f"<h2>{post['title']}</h2>"
+        html_content += f"<p>{post['description']}</p>"
+        html_content += "</div>"
+        html_content += "</div>"
+    
+    html_content += "</body></html>"
+    
+    with open('frontend/templates/profiles.html', 'w') as html_file:
         html_file.write(html_content)
 
 description = "hij"
@@ -60,7 +77,7 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
-    generate_html(response) 
+    generate_feed(response) 
     #print([render_template('postTemplate.html', **r) for r in response])
     #html = '\n'.join([render_template('postTemplate.html', **r) for r in response])
     #with open(r'.\templates\out.html', 'w') as f:
@@ -95,9 +112,10 @@ def createpost():
 
 @app.route("/account/")
 def account():
+    generate_profile(response)    
     return render_template("account.html")
 
 if __name__ == "__main__":
-    app.register_blueprint(api)
+    #app.register_blueprint(api)
     app.run()
                                      
